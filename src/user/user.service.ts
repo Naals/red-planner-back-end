00cab7cecfffff6086e09ec4ed7f67af61b78182
Aslan <tynyshtykaslan06@gmail.com>
2025-10-30 19:13:38 +0500

    feat: initialize Prisma schema and set up user authentication module
    - Added Prisma schema for User, Task, TimeBlock, PromodoroSession, and PromodoroRound models.
    - Implemented AuthModule with AuthController and AuthService for user authentication.
    - Created JWT strategy for authentication using Passport.
    - Configured JWT settings in a separate configuration file.
    - Set up main application module with global prefix and CORS settings.
    - Developed UserModule with UserController and UserService for user management.
    - Added DTOs for creating and updating users.
    - Implemented basic e2e tests for application.
    - Configured TypeScript settings for the project.

---
+++ src/user/user.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@nestjs/common'
+import { CreateUserDto } from './dto/create-user.dto'
+import { UpdateUserDto } from './dto/update-user.dto'
+
+@Injectable()
+export class UserService {
+	constructor(private prisma: PrismaService) { }
+
+	getById(id: number) {
+		return this.prisma.user.findUnique({ where: { id } }, { include: { tasks: true } })
+	}
+
+}
